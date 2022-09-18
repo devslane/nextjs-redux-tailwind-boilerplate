ed9aac6c4a88dd949e5e6276ddf54bf2d3ea1ee3
Madhav Bhasin <madhav.bhasin@devslane.com>
2022-09-18 13:24:03 +0530

    REFACTOR: remove unused code from _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import App, { AppContext, AppInitialProps } from 'next/app';
+import App, { AppInitialProps } from 'next/app';
 import wrapper from '../src/redux/store';
 import '../src/index.scss';
 
 class WrappedApp extends App<AppInitialProps> {
-  public static getInitialProps = async ({ Component, ctx }: AppContext) => ({
-    pageProps: {
-      ...(Component.getInitialProps
-        ? await Component.getInitialProps(ctx)
-        : {}),
-      appProp: ctx.pathname,
-    },
-  });
-
   public render() {
     const { Component, pageProps } = this.props;
     return <Component {...pageProps} />;
