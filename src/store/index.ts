907ef921da6aea4be1b111e897c4ab4f4be17912
Madhav Bhasin <madhav.bhasin@devslane.com>
2022-12-25 13:47:10 +0530

    Update dependency

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,7 @@
 // store.ts
-
+import { configureStore } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
-import { applyMiddleware, createStore, Store } from 'redux';
-import { composeWithDevTools } from '@redux-devtools/extension';
+import { Store } from 'redux';
 import createSagaMiddleware, { Task } from 'redux-saga';
 import RootReducer, { AppState } from './reducers';
 import rootSaga from './sagas';
@@ -17,13 +16,15 @@ const makeStore = () => {
   const sagaMiddleware = createSagaMiddleware();
 
   // 2: Add an extra parameter for applying middleware:
-  const store = createStore(
-    RootReducer,
-    composeWithDevTools(applyMiddleware(sagaMiddleware))
-  );
+  const store = configureStore({
+    reducer: RootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(sagaMiddleware),
+    devTools: process.env.NODE_ENV !== 'production',
+  });
 
   // 3: Run your sagas on server
-  (store as SagaStore).sagaTask = sagaMiddleware.run(rootSaga);
+  sagaMiddleware.run(rootSaga);
 
   // 4: now return the store:
   return store;
@@ -31,5 +32,5 @@ const makeStore = () => {
 
 // export an assembled wrapper
 export default createWrapper<Store<AppState>>(makeStore, {
-  debug: true,
+  debug: process.env.NODE_ENV !== 'production',
 });
